c5e3a4a4a6ad3171be598db22278237d066f8a68
nhudh <nhudh21411@st.uel.edu.vn>
2023-12-14 00:48:34 +0700

    user_1den5

--- models/user.js
+++ models/user.js
@@ -1,8 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require( 'mongoose')
 
-const userSchema = mongoose.Schema({
-    name: String,
-    image: String,
-})
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: Number,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    district: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        default: '',
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+});
+userSchema.virtual('user_id').get(function () {
+    return this._id.toHexString();
+});
+
+userSchema.set( 'toJSON', {
+    virtuals: true,
+});
 
 exports.User = mongoose.model('User', userSchema);
+exports.userSchema = userSchema;
