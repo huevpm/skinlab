0369e963191e28754ca19ae301d8adb22a0027ad
vophucthao2908 <phucthaov@gmail.com>
2023-12-28 07:59:03 +0700

    login 5

--- libs/users/src/lib/models/services/auth-guard.service.ts
+++ libs/users/src/lib/models/services/auth-guard.service.ts
@@ -14,11 +14,14 @@ export class AuthGuard implements CanActivate {
         const token = this.localStorageToken.getToken();
 
         if(token) {
-            return true
+            const tokenDecode = JSON.parse(atob(token.split('.')[1]));
+            if (tokenDecode.isAdmin && !this._tokenExpired(tokenDecode.exp)) return true;
         }
-        
         this.router.navigate(['/login']);
         return false;
 
+    }
+    private _tokenExpired(expiration): boolean {
+        return Math.floor(new Date().getTime() / 1000) >= expiration;
     }
 }
