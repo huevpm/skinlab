ffa90c342b39916b0361508e8acd4b576cb811cc
vophucthao2908 <phucthaov@gmail.com>
2023-12-30 12:36:51 +0700

    login

--- libs/users/src/index.ts
+++ libs/users/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/models/services/auth-guard.service';
 export * from './lib/models/users.module';
 export * from './lib/models/services/users.services';
 export * from './lib/models/services/jwt.interceptor';
+export * from './lib/models/services/auth.service';
